7a5c1070cfde05ebe4ba06adffc3139ea7162210
unknown <davidonzan@gmail.com>
2022-07-22 22:47:10 +0100

    Test Frontend major update

--- portal/static/js/create-test.js
+++ portal/static/js/create-test.js
@@ -1,55 +1,54 @@
-var questions = document.getElementById('questions');
-var addOption = document.querySelectorAll('.add_option');
-var addQuestion = document.getElementById('add_question');
-const questionTemplate = document.querySelector("[question-template]")
-const optionTemplate = document.querySelector("[option-template]")
-// var remove_fields = document.getElementById('remove_fields');
+var questions = document.getElementById("questions");
+var addQuestion = document.getElementById("add_question");
+const questionTemplate = document.querySelector("[question-template]");
+const optionTemplate = document.querySelector("[option-template]");
 
 addQuestion.addEventListener("click", () => {
   let newQuestion = questionTemplate.content.cloneNode(true).children[0];
-  questions.append(newQuestion)
-  addOptionListener(newQuestion.querySelector(".add_option"))
-})
+  const idx = questions.childElementCount + 1;
+  newQuestion.setAttribute("questionNumber", idx);
+  newQuestion.querySelector("input[type=text]").setAttribute("name", idx);
+  questions.append(newQuestion);
+  newQuestion
+    .querySelector(".q-delete-btn")
+    .addEventListener("click", (e) => newQuestion.remove());
+  addOptionListener(newQuestion.querySelector(".add_option"));
+});
 
 function addOptionListener(optionBtn) {
   optionBtn.addEventListener("click", (e) => {
-    let newOption = optionTemplate.content.cloneNode(true).children[0]
-    e.target.previousElementSibling.append(newOption)
-  })
+    let newOption = optionTemplate.content.cloneNode(true).children[0];
+    const idx = `${optionBtn.parentElement.getAttribute("questionNumber")}-${
+      optionBtn.previousElementSibling.childElementCount + 1
+    }`;
+    optionBtn.previousElementSibling.append(newOption);
+    newOption.setAttribute("optionNumber", idx);
+    newOption.querySelector("input[type=text]").setAttribute("name", idx);
+    newOption.querySelector(".q-delete-btn").addEventListener("click", (e) => newOption.remove());
+    const choose_ansBtn = newOption.querySelector(".choose-ans");
+    choose_ansBtn.addEventListener("click", addChooseAnswer)
+  });
 }
 
-addOption.forEach(optionBtn =>
-  optionBtn.addEventListener("click", (e) => {
-    let newOption = optionTemplate.content.cloneNode(true).children[0]
-    e.target.previousElementSibling.append(newOption)
-}))
+function addChooseAnswer(e) {
+    let questionOptions = this.parentElement.parentElement.parentElement.children;
+    for (let i = 0; i < questionOptions.length; i++) {
+      questionOptions[i].querySelector(".choose-ans").setAttribute("hidden", "true");
+      questionOptions[i].querySelector(".q-delete-btn").setAttribute("disabled", "true");
+    }
+    this.removeAttribute("hidden");
+    this.querySelector("i").innerHTML = "check_circle";
+    this.removeEventListener("click", addChooseAnswer)
+    this.addEventListener("click", addRemoveAnswer)
+}
 
-
-// remove_fields.onclick = function(){
-//   var input_tags = survey_options.getElementsByTagName('input');
-//   if(input_tags.length > 2) {
-//     survey_options.removeChild(input_tags[(input_tags.length) - 1]);
-//   }
-// }
-
-
-// var survey_options = document.getElementById('survey_options');
-// var add_more_fields = document.getElementById('add_more_fields');
-// var remove_fields = document.getElementById('remove_fields');
-
-// add_more_fields.onclick = function(){
-//   var newField = document.createElement('input');
-//   newField.setAttribute('type','text');
-//   newField.setAttribute('name','survey_options[]');
-//   newField.setAttribute('class','survey_options');
-//   newField.setAttribute('siz',50);
-//   newField.setAttribute('placeholder','Another Field');
-//   survey_options.appendChild(newField);
-// }
-
-// remove_fields.onclick = function(){
-//   var input_tags = survey_options.getElementsByTagName('input');
-//   if(input_tags.length > 2) {
-//     survey_options.removeChild(input_tags[(input_tags.length) - 1]);
-//   }
-// }
+function addRemoveAnswer(e) {
+    let questionOptions = this.parentElement.parentElement.parentElement.children;
+    for (let i = 0; i < questionOptions.length; i++) {
+      questionOptions[i].querySelector(".choose-ans").removeAttribute("hidden");
+      questionOptions[i].querySelector(".q-delete-btn").removeAttribute("disabled");
+    }
+    this.querySelector("i").innerHTML = "done_outline";
+    this.removeEventListener("click", addRemoveAnswer)
+    this.addEventListener("click", addChooseAnswer)
+}
